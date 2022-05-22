03b14b815bc07b2ab406cd06e752865f04098ab2
jbryans <jbryans2014@gmail.com>
2022-05-22 16:15:25 -0500

    two test works, hints and node acting weird

--- main.js
+++ main.js
@@ -28,13 +28,63 @@ const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-const generateHint = () =>  {
-  // your code here
+const generateHint = (guess) =>  {
+  // create variables solutionArray and guessArray that each split up passed in arguments, [.split] on empty string ('')
+  let solutionArray = guess.split('');
+  let guessArray = solution.split('');
+
+  // Create a variable correctLetterLocations and set it to 0. This variable will record how many correct "letter-locations" were guessed.
+  let correctLetterLocations = 0;
+
+  let correctLetters = 0;
+
+  //In a for loop, iterate over the solutionArray, comparing each index of solutionArray against the same index of guessArray
+  for (let i = 0; i < 4; i++){
+    if (guessArray[i] === solutionArray[i]){
+
+      //If the item matches, increment correctLetterLocations, and set that index in solutionArray to null.
+      correctLetterLocations++;
+      solutionArray[i] = null;
+
+    }
+  }
+  // console.log(solutionArray);
+  // console.log(guessArray);
+  //Set a variable correctLetters equal to 0, and in a for loop, again iterate over the solutionArray. Using .indexOf, determine if the item at the current index in guessArray appears inside of solutionArray
+  for (let i=0; i<4; i++) {
+    //Save that index in a variable called targetIndex
+    let targetIndex = solutionArray.indexOf(guessArray[i]); 
+    //Now, if targetIndex is greater than -1 increment correctLetters and set the item in solutionArray at that index equal to null.
+    if (targetIndex > -1) {
+      correctLetters++;
+      solutionArray[targetIndex] = null;
+      // console.log(solutionArray);
+  }
+}
+// return a string that prints out the hints you generated, with correctLetterLocations being red, correctLetters being white, and separated by a hyphen. 
+return correctLetterLocations + "-" + correctLetters;
+
 }
 
+
 const mastermind = (guess) => {
-  solution = 'abcd'; // Comment this out to generate a random solution
-  // your code here
+  // solution = 'abcd'; // Comment this out to generate a random solution
+  // add guess and hint to the board
+  if (guess == solution) {
+    console.log("You guessed it!")
+    console.log("Resetting game.")
+    board = []
+    return "You guessed it!"
+  } else {
+    hint = generateHint(guess)
+    if (board.length == 10) {
+      console.log(`You ran out of turns! The solution was ${solution}`)
+    } else {
+      board.push(guess.concat(" ", hint))
+      console.log("Guess again.")
+    }
+  }
+
 }
 
 
